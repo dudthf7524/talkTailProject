8c78657803aebf44ef2c7fa0add97404f0bdd5f2
user <dudthf7524@naver.com>
2025-04-15 13:10:35 +0900

    s

--- src/Page/Home/homeGuideDetail.js
+++ src/Page/Home/homeGuideDetail.js
@@ -5,12 +5,23 @@ const HomeGuideDetail = () => {
   return (
     <div className="homeGuideDetail">
       <img src="/guide/001.png" alt="" />
-      <img src="/guide/002.png" alt="" />
       <img src="/guide/003.png" alt="" />
       <img src="/guide/004.png" alt="" />
       <img src="/guide/005.png" alt="" />
       <img src="/guide/006.png" alt="" />
       <img src="/guide/007.png" alt="" />
+      <img src="/guide/008.png" alt="" />
+      <img src="/guide/009.png" alt="" />
+      <img src="/guide/010.png" alt="" />
+      <img src="/guide/011.png" alt="" />
+      <img src="/guide/012.png" alt="" />
+      <img src="/guide/013.png" alt="" />
+      <img src="/guide/014.png" alt="" />
+      <img src="/guide/015.png" alt="" />
+      <img src="/guide/016.png" alt="" />
+      <img src="/guide/017.png" alt="" />
+      <img src="/guide/019.png" alt="" />
+      
     </div>
   );
 };
